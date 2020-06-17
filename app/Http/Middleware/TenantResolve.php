99d4a03fb4c4703c80171f73793680102c4297f4
Rushio <rushio@orba.io>
2020-06-16 23:27:32 -0500

    domain resolve lolahost

--- app/Http/Middleware/TenantResolve.php
+++ app/Http/Middleware/TenantResolve.php
@@ -17,27 +17,39 @@ class TenantResolve
     public function handle($request, Closure $next)
     {
         $resolved = false;
+        $check = 'subdomain';
         //try to resolve otherwise
 
-        $domain = $request->getHost();
-        $domain = str_replace("www.", '', $domain); //get rid of www.
-        $domain = str_replace(".".env("MAIN_HOST"), '', $domain);
-        //subdomain
-        $tenant = Tenant::where('subdomain', $domain)->orWhere('domain', $domain)->first(); 
-        if($tenant!=null)
-        {
-            $request->merge(['tenant_id'=>$tenant->id]);
-            $resolved = true;
-        }
+            $part = $request->header("LolaHost");
 
-        if($resolved)
-        {
-            //set cookie domain and accept next request
-            \Config::set("session.domain", $domain);
-            return $next($request);
-        }
-        //redirect away to 404
-        return redirect()->away(env("APP_URL").'/404');
+            if($part!=null)
+                $check = 'domain';
+                
+            if($check=='subdomain')
+            {
+                $domain = $request->getHost();
+                $domain = str_replace("www.", '', $domain); //get rid of www.
+                $domain = str_replace(".".env("MAIN_HOST"), '', $domain);
+                //subdomain
+                $parts = explode(".", $domain);
+                $part = $parts[0];
+            }
+            $tenant = Tenant::where($check, $part)->first(); 
+            if($tenant!=null)
+            {
+                $request->merge(['tenant_id'=>$tenant->id]);
+                $resolved = true;
+            }
+
+            if($resolved)
+            {
+                //set cookie domain and accept next request
+                \Config::set("session.domain", $domain);
+                return $next($request);
+            }
+            //redirect away to 404
+            return redirect()->away(env("APP_URL").'/404');
+
 
     }
 
