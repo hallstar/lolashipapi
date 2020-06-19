45619194412ade9b23be93b4de6f7225e736aaaa
Rushio <rushio@orba.io>
2020-06-19 16:02:29 -0500

    livewire

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -9,7 +9,7 @@
     />
     <link href="{{ mix('css/app.css') }}" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;0,800;1,200;1,400&display=swap" rel="stylesheet">
-    <livewire:styles>
+    @livewireStyles
   
   </head>
   <body>
@@ -40,6 +40,6 @@
       </div>
     </div>
     </div>
-    <livewire:scripts>
+    @livewireScripts
   </body>
 </html>
